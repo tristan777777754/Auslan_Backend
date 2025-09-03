9805be654c1ab412f0de1a9931f8f840320d9571
tristan0721 <shua0140@student.monash.edu>
2025-09-03 11:44:21 +1000

    delete_viloin hostname

--- violin_visual.py
+++ violin_visual.py
@@ -20,11 +20,11 @@ from dotenv import load_dotenv
 load_dotenv()  # load .env file
 print("DB_HOST from env =", os.getenv("DB_HOST"))
 
-DB_HOST = os.getenv("DB_HOST", "database-auslan.c9yoaa6kcl93.ap-southeast-2.rds.amazonaws.com")
-DB_PORT = int(os.getenv("DB_PORT", "3306"))
-DB_NAME = os.getenv("DB_NAME", "myapp_db")
-DB_USER = os.getenv("DB_USER", "tristan")
-DB_PASS = os.getenv("DB_PASS", "Auslan47")
+DB_HOST = os.getenv("DB_HOST", "")
+DB_PORT = int(os.getenv("DB_PORT", ""))
+DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASS = os.getenv("DB_PASS", "")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 

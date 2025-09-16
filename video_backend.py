ac9a93ba468e08325b39f09cec3db5d2558c6d92
tristan0721 <shua0140@student.monash.edu>
2025-09-16 18:32:48 +1000

    chage to use pre signed URL

--- video_backend.py
+++ video_backend.py
@@ -22,23 +22,21 @@ S3_BUCKET = os.getenv("S3_BUCKET", "demo2109bhargav")
 
 s3 = boto3.client("s3", region_name=AWS_REGION)
 
-# ---------- API: Get video list ----------
+# ---------- API: Get all videos with pre-signed URL ----------
 @router.get("/")
 def get_videos():
-  
     with engine.connect() as conn:
         result = conn.execute(text("SELECT id, filename, s3_key FROM videos"))
-        videos = [dict(row._mapping) for row in result]
+        videos = []
+        for row in result:
+            video = dict(row._mapping)
+
+            #  pre-signed URL
+            url = s3.generate_presigned_url(
+                "get_object",
+                Params={"Bucket": S3_BUCKET, "Key": video["s3_key"]},
+                ExpiresIn=31536000  # API URL valid for 1 year
+            )
+            video["url"] = url
+            videos.append(video)
     return videos
-
-
-# ---------- API: Get video presigned URL ----------
-@router.get("/{video_key}")
-def get_video_url(video_key: str):
-   
-    url = s3.generate_presigned_url(
-        "get_object",
-        Params={"Bucket": S3_BUCKET, "Key": video_key},
-        ExpiresIn=3600  # 1小時
-    )
-    return {"url": url}
